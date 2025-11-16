8a1a985363f672192e743e7843581a04b6e1c8f3
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-11-16 23:57:28 +0530

    refactor(backend): remove unused imports and integrate WebSocket service

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,51 +1,14 @@
 import "dotenv/config";
 import express from "express";
-import { ChatAnthropic } from "@langchain/anthropic";
-import { MessagesAnnotation, StateGraph } from "@langchain/langgraph";
-import { add_dependency, createToolsWithContext, hello_world } from "./tools/tool.js";
-import { Sandbox } from "@e2b/code-interpreter";
-import { createAgent } from "langchain";
+import { setupWebSocket } from "./services/websocket.service.js";
 
 const app = express();
-
 app.use(express.json());
-
-const llm = new ChatAnthropic({
-  apiKey: process.env.ANTHROPIC_API_KEY || "",
-  model: "claude-3-7-sonnet-20250219",
-});
-
-
 
 const port = 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-
-const sbx = await Sandbox.create();
-const execution = await sbx.runCode('print("hello world")');
-// console.log(execution.logs);
-
-// const files = await sbx.files.list("/");
-// console.log(files);
-
-
-
-const tools = createToolsWithContext({
-  sandbox: sbx,
-  socket: null,
-  projectId: "123",
-});
-
-const agent = createAgent({
-  model: llm,
-  tools: tools,
-});
-
-const result = await agent.invoke({
-  messages: [{ role: "user", content: "create a new file called app.tsx with the content 'console.log(\"Hello, world!\")'" }]
-});
-
-console.log("Final result:", result);
+setupWebSocket(server);
